c3bd6e16babdf86fa45df88c9818abfb45ff9899
Alessandro Candido <candido.ale@gmail.com>
2021-01-26 17:13:14 +0100

    Update magic (dynamic) import in setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
+import sys
 import pathlib
-import importlib.util
+import importlib
 
 from setuptools import setup, find_packages
 
 MAJOR = 0
 MINOR = 0
-MICRO = 1
+MICRO = 2
 repo_path = pathlib.Path(__file__).absolute().parent
 
-versions_spec = importlib.util.spec_from_file_location(
-    "packutil.versions", "src/packutil/versions.py"
-)
-versions = importlib.util.module_from_spec(versions_spec)
-versions_spec.loader.exec_module(versions)
+# import packutil as pu
+sys.path.insert(0, str((repo_path / "src").absolute()))
+pu = importlib.import_module("packutil")
+sys.path.pop(0)
 
 
 def setup_package():
     # write version
-    versions.write_version_py(
+    pu.versions.write_version_py(
         MAJOR,
         MINOR,
         MICRO,
-        versions.is_released(repo_path),
+        pu.versions.is_released(repo_path),
         filename="src/packutil/version.py",
     )
     # paste Readme
@@ -32,7 +32,7 @@ def setup_package():
     setup(
         name="packutil",
         author="Alessandro Candido",
-        version=versions.mkversion(MAJOR, MINOR, MICRO),
+        version=pu.versions.mkversion(MAJOR, MINOR, MICRO),
         long_description=long_description,
         long_description_content_type="text/markdown",
         author_email="",
