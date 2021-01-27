69d0e8c6c85f575b54160ffda9fe21c27e7480db
Alessandro Candido <candido.ale@gmail.com>
2021-01-27 11:23:03 +0100

    Introduce new standard abbreviation

--- setup.py
+++ setup.py
@@ -10,19 +10,19 @@ MINOR = 0
 MICRO = 2
 repo_path = pathlib.Path(__file__).absolute().parent
 
-# import packutil as pu
+# import packutil as pack
 sys.path.insert(0, str((repo_path / "src").absolute()))
-pu = importlib.import_module("packutil")
+pack = importlib.import_module("packutil")
 sys.path.pop(0)
 
 
 def setup_package():
     # write version
-    pu.versions.write_version_py(
+    pack.versions.write_version_py(
         MAJOR,
         MINOR,
         MICRO,
-        pu.versions.is_released(repo_path),
+        pack.versions.is_released(repo_path),
         filename="src/packutil/version.py",
     )
     # paste Readme
@@ -32,7 +32,7 @@ def setup_package():
     setup(
         name="packutil",
         author="Alessandro Candido",
-        version=pu.versions.mkversion(MAJOR, MINOR, MICRO),
+        version=pack.versions.mkversion(MAJOR, MINOR, MICRO),
         long_description=long_description,
         long_description_content_type="text/markdown",
         author_email="",
